92c9363484c640a142dd50d0dee86b6ccd3b1742
Jeliseo02 <jackeliseo02@gmail.com>
2024-10-29 16:00:43 -0400

    Added functional lobbies and styling to the lobbies, multiple players can now join a lobby

--- src/routes/game.js
+++ src/routes/game.js
@@ -6,20 +6,102 @@ const Lobby = require('../models/lobby')
 
 // Socket.IO connection handler
 const handleSocketConnection = (io) => {
-    io.on('connection', (socket) => {
-        console.log('New Client Connected!');
 
-        socket.on('PlayerJoin', (playerData) => {
-            console.log('Player Joined: ', playerData);
-            // TODO: Implement Player Join Logic
+    const lobbyConnections = new Map(); // Using Map to store lobby connections data
+
+    //Handle joining a lobby
+    io.on('connection', (socket) => {
+        console.log('New Client Connected! Socket ID: ', socket.id);
+
+        socket.on('joinLobby', async (data) => {
+            const {lobbyId, username, userId} = data;
+            console.log(`Socket ${socket.id} joining lobby: ${lobbyId}`);
+            console.log('Join lobby data: ', {lobbyId, username, userId});
+
+            //Emit the player list to all clients in the lobby
+            try {
+
+                let lobby = await Lobby.findOne({urlId: lobbyId})
+                    .populate({
+                        path: 'players.userId',
+                        select: 'username' 
+                    })
+                    .populate('hostId')
+                console.log('Initial lobby players: ', lobby.players);
+
+                //Check to make sure plater isn't aleady in the lobby
+                if(!lobby.players.some(player => player.userId.equals(userId))) {
+                    await lobby.addPlayer(userId);
+
+                    //Refresh lobby data after adding player
+                    lobby = await Lobby.findOne({urlId: lobbyId})
+                        .populate({
+                            path: 'players.userId',
+                            select: 'username'
+                        })
+                        .populate('hostId');
+                    console.log('Updated lobby players: ', lobby.players)
+                }
+
+                //Joining the Socket.IO room for this lobby
+                socket.join(lobbyId);
+
+                //Tracking connection for the lobby
+                if (!lobbyConnections.has(lobbyId)) {
+                    lobbyConnections.set(lobbyId, new Set());
+                }
+
+                //Add socket.id to connections
+                lobbyConnections.get(lobbyId).add(socket.id);
+
+                // Add debugging log to see what we're emitting
+                const playerData = lobby.players.map(player => ({
+                    username: player.userId.username,
+                    isHost: player.userId.equals(lobby.hostId)
+                }));
+                console.log('Emitting player data:', playerData);
+
+            
+                //Emit the current player list with refreshed data
+                io.to(lobbyId).emit('updatePlayerList', {
+                    players: lobby.players.map(player => ({
+                        username: player.userId.username,
+                        isHost: player.userId.equals(lobby.hostId)
+                    }))
+                });
+
+                //Emit the current number of connections to all clients in lobby
+                const connectionCount = lobbyConnections.get(lobbyId).size;
+                io.to(lobbyId).emit('connectionUpdate', {
+                    count: connectionCount
+                });
+    
+            } catch (error) {
+                console.error('Error updating player list: ', error);
+            }
         });
 
         socket.on('disconnect', () => {
-            console.log('Client Disconnected');
-            // TODO: Handle player disconnect logic
+            console.log('Client Disconnected, socketId: ', socket.id);
+
+            lobbyConnections.forEach((connections, lobbyId) => {
+                if (connections.has(socket.id)) {
+                    connections.delete(socket.id);
+                    io.to(lobbyId).emit('connectionUpdate', {
+                        count: connections.size
+                    });
+                }
+            });
         });
     });
 };
+
+
+
+
+
+
+
 
 //  - - - - - Lobby Route - - - - - 
 router.get('/lobby', isAuthenticated, (req, res) => {
